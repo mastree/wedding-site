36e0aa9c595ff1ba65c88d90c3561bd8c868c167
Kamal Shafi <muha.kamal@gmail.com>
2024-06-15 22:26:12 +0800

    feat: add schedule and location section content

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   template: `
-    <div class="rounded-lg h-[35%] w-[90%] bg-slate-100 shadow-md"></div>
+    <div class="flex flex-row items-center rounded-lg min-h-40 p-5 w-[90%] bg-slate-100 shadow-md">
+      <div class="h-full min-w-[25%] max-w-[35%] flex flex-col justify-center items-center">
+        <img class="max-h-32 max-w-[4.5rem]" [src]="icon" />
+      </div>
+      <div class="grow flex flex-col gap-2 pl-3 h-full items-center justify-center">
+        <div class="grow flex justify-center items-center">
+          <p class="font-manuale font-semibold text-xl">{{ title }}</p>
+        </div>
+        <div class="grow-[2] w-full">
+          <ng-content></ng-content>
+        </div>
+      </div>
+    </div>
   `,
   styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() icon: string = "";
+  @Input() title: string = "";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,9 +10,9 @@ TODO:
 */
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    template: `
+  selector: 'app-home',
+  standalone: true,
+  template: `
     <header class="h-[66vh] bg-white">
       <app-navigation-bar></app-navigation-bar>
       <div>
@@ -74,7 +74,7 @@ TODO:
         </a>
       </div>
       <div
-        class="flex gap-0 justify-center items-center absolute bottom-0 mb-3 w-full"
+        class="flex gap-0 justify-center items-center absolute bottom-0 mb-1 w-full"
       >
         <div class="size-8 rounded-[50%]">
           <svg
@@ -108,14 +108,25 @@ TODO:
       </div>
     </section>
 
-    <section class="h-[100vh] py-16 gap-8 bg-white flex flex-col">
+    <section class="pt-16 pb-20 gap-8 bg-white flex flex-col">
       <div class="flex flex-col justify-center">
         <p class="font-manuale text-xl font-semibold text-center">SCHEDULE & LOCATION</p>
         <p class="font-manuale text-md font-light text-center">Jadwal & Tempat</p>
       </div>
       <div class="flex flex-col flex-grow gap-8 justify-start items-center">
-        <app-card></app-card>
-        <app-card></app-card>
+        <app-card icon="calendar.svg" title="Schedule">
+          <p class="font-manuale text-[1rem]"><span class="font-semibold">Date:</span> Saturday, 16 Nov 2024</p>
+          <p class="font-manuale text-[1rem]"><span class="font-semibold">Time:</span> 15.00 &mdash; 19.00 WIB</p>
+        </app-card>
+        <app-card icon="location-pin.svg" title="Location">
+          <p class="font-manuale text-[1rem] font-semibold">Maxi's Resto</p>
+          <p class="font-manuale text-[1rem]">
+          Jl. Gunung Agung No.8, Ciumbuleuit, Kota Bandung
+          </p>
+          <a class="text-sky-400" href="https://maps.app.goo.gl/9kLNCJA7acvwyxJB8">
+            <p class="mt-2">Open in GMaps</p>
+          </a>
+        </app-card>
       </div>
     </section>
 
@@ -126,7 +137,7 @@ TODO:
       <p class="text-3xl font-manuale">
     </section>
   `,
-    styleUrl: './home.component.css',
-    imports: [NavigationBarComponent, CountdownComponent, CardComponent]
+  styleUrl: './home.component.css',
+  imports: [NavigationBarComponent, CountdownComponent, CardComponent]
 })
-export class HomeComponent {}
+export class HomeComponent { }
