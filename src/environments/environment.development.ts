e06285730406951bd3cba16c946076725c3dc291
Kamal Shafi <muha.kamal@gmail.com>
2024-07-09 23:42:29 +0800

    env: change base url temporarily

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ import { LogLevel } from '../app/log-level.enum';
 import { Environment } from './types';
 
 export const environment: Environment = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://localhost/api',
   LOG_LEVEL: LogLevel.DEBUG,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import { Environment } from './types';
 
 export const environment: Environment = {
   // TODO: provide prod url before deployment
-  API_URL: 'http://localhost/api',
+  API_URL: 'http://faiza.kamalshafi.me/api',
   LOG_LEVEL: LogLevel.INFO,
 };
