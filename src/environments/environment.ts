f03cc7331d6b41a6acdb980dddb9c89ea42285df
Kamal Shafi <muha.kamal@gmail.com>
2024-07-11 23:50:53 +0800

    fix: change backend url to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ import { LogLevel } from '../app/log-level.enum';
 import { Environment } from './types';
 
 export const environment: Environment = {
-  // TODO: provide prod url before deployment
-  API_URL: 'http://faiza.kamalshafi.me/api',
+  API_URL: 'https://faiza.kamalshafi.me/api',
   LOG_LEVEL: LogLevel.INFO,
 };
